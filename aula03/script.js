31942a4cab6c293fcef8a7167901fcec2230466b
xiaoRuka <rukamatsu@gmail.com>
2023-07-10 12:01:42 -0300

    <Feat>

---
+++ aula03/script.js
@@ -0,0 +1,7 @@
+
+var n1 = Number.parseInt(window.prompt("Digite um número"))
+var n2 = Number.parseInt(window.prompt("Digite outro número"))
+
+window.alert(`A soma dos dois valores é ${n1 + n2}`)
+
+// Para transformar uma string para número podemos utilizar Number.parseInt ou Number.parseFloat, ou o comando Number também funciona
