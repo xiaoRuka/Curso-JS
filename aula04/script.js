443c321c2efd80606d67026e54948631d8b06cdd
xiaoRuka <rukamatsu@gmail.com>
2023-07-11 16:42:46 -0300

    <Fix>: aula04

--- aula04/script.js
+++ aula04/script.js
@@ -2,3 +2,4 @@
 let nome = window.prompt('Qual é o seu nome?')
 document.write(`Seu nome tem ${nome.length} letras <br/>`)
 document.write(`Seu nome em letras maiúsculas fica ${nome.toUpperCase()}`)
+
